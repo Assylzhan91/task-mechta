82cb15a7598d65204f5982ed2e84337223d4bc10
Assylzhan <assylzhan.baimuratov@gmail.com>
2021-09-30 16:51:10 +0600

    Refactoring: Search for buttons is done

--- src/views/VSearch.vue
+++ src/views/VSearch.vue
@@ -4,8 +4,6 @@
 		@submit.prevent = "onSubmit"
 		@keypress.enter = "onSubmit"
 	>
-		<h1>showError: {{ showError }}</h1>
-		<h1>showErrorSearchVal: {{ showErrorSearchVal }}</h1>
 		<div
 			class="form__group"
 			:class="setClassShow"
@@ -13,23 +11,35 @@
 			<v-input
 				:value="searchVal"
 				@handler="searchSetVal"
+				:class="!showError && btnTextError && btnTextClear ? 'form__input-error':''"
 			/>
-			<v-btn
-				:colorType="btnTypeColor "
-				class="form__btn"
-				@handler="btnClicks"
-			>{{ textBtn }}</v-btn>
+			<template>
+				<v-btn
+					v-if="!searchVal.length || !btnTextClear || showError"
+					colorType="sky-blue"
+					class="form__btn"
+					:class="!btnTextClear ? 'form__btn-clear':''"
+					@handler="btnTextClear ? onSubmit() : clearValCityAction()"
+				>{{ btnTextClear ? 'Enter': 'X' }}</v-btn>
+				<v-btn
+					v-else-if="!showError && btnTextError && btnTextClear"
+					colorType="scarlet"
+					class="form__btn"
+					@handler="clearValCityAction"
+				>X</v-btn>
+			</template>
+
 			<template v-if="isOpen">
 				<ul class="form__list" v-if="showError">
 					<template v-for="city of searchingCityVal" >
-						<li class="form__item" @click="()=>selectedCity(city.id)" :key="city.id">
+						<li class="form__item" @click="selectedCity(city.id)" :key="city.id">
 							{{ city.label }}
 						</li>
 					</template>
 				</ul>
 			</template>
 			<strong
-				v-if="showError || searchVal.length && showErrorSearchVal"
+				v-if="showError || searchVal.length && btnTextClear"
 				class="form__error"
 			>
 				We didn’t found such city. Please check spelling
@@ -48,6 +58,8 @@ import { mapGetters, mapActions, mapState } from "vuex";
 export default {
 	data: ()=>({
     showError: false,
+    btnTextClear: true,
+    btnTextError: false,
 	}),
   components: {
     VInput,
@@ -80,35 +92,40 @@ export default {
     }),
 
 		btnClicks() {
-      return (!this.searchingCityVal.length && this.searchVal.length)
+     /* return (!this.searchingCityVal.length && this.searchVal.length)
 				? this.clearValCityAction
-				: this.onSubmit
+				: this.onSubmit*/
 		},
     showErrorSearchVal() {
-      return this.searchVal.length && !this.searchingCityVal.length
+      // return this.searchVal.length && !this.searchingCityVal.length
 		},
 		btnTypeColor (){
-      return  this.showErrorSearchVal ? 'scarlet' : 'sky-blue'
+      // return  this.showErrorSearchVal ? 'scarlet' : 'sky-blue'
 		},
 		textBtn() {
-      return  this.showErrorSearchVal ? 'X' : 'Enter'
+      // return  this.showErrorSearchVal ? 'X' : 'Enter'
 		}
 	},
 	watch: {
     searchVal(val) {
-     if(val.length && this.searchingCityVal.length) {
-       if(this.isOpen && this.searchingCityVal.length) {
-         if(this.searchingCityVal[0].label === val){
-           this.showError = false
-         }else {
-           this.showError = true
-				 }
-       }else {
-         this.showError = false
-			 }
-     }else {
-       this.showError = false
-		 }
+      if(this.searchingCityVal.length && this.searchingCityVal[0].label === val) {
+        this.showError = false
+        this.btnTextClear = false
+			}else {
+        this.btnTextClear = true
+        if(val.length && this.searchingCityVal.length) {
+          this.showError = true
+        }else {
+          this.btnTextError = true
+          // this.btnTextClear = false
+					if(val.length) {
+            this.btnTextError = true
+					}else {
+            this.btnTextError = false
+					}
+          this.showError = false
+				}
+			}
 		}
 	}
 }
